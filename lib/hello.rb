add958cd3357bf4888df2be780ebc2bd8d60e383
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 03:12:23 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,22 @@
 def hello_t(names)
+  if block_given? 
    badge = 0 
+   
    while badge < names.length 
    yield(names[badge])
    badge = badge + 1 
- end 
+ end
+ 
+ 
  badge
+ else 
+   puts "Hey! No block was given!"
+ end 
 end
 
 
      
-hello_t(["Tim", "Tom", "Jim"]).each do |name|
+hello_t [names].each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end 
