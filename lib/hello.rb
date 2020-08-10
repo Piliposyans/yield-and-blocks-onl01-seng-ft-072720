a3320a0fe0a11df7d9337f1a20d8acc140cd00ff
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 03:22:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -15,7 +15,7 @@ end
 
 ["Tim", "Tom", "Jim"]
      
- hello_t(["Tim", "Tom", "Jim"]).each do |name|
+def hello_t(["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end 
