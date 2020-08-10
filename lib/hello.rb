30b92cc49ca342282bfa0b7971bc11643efcf34b
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 03:06:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,12 +9,11 @@ end
 
 
      
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end 
 end 
 
-def hello_t(names)
-  badge = 0 
+
   
